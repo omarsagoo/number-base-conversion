ea3dc2d9a715635eba86931fc6d079961c9070d2
omarsagoo <om.sagoo@gmail.com>
2020-01-25 15:20:41 -0800

    complete decode, encode, and convert functions

--- bases.py
+++ bases.py
@@ -1,13 +1,5 @@
-#!python
-
+from math import modf
 import string
-# Hint: Use these string constants to encode/decode hexadecimal digits and more
-# string.digits is '0123456789'
-# string.hexdigits is '0123456789abcdefABCDEF'
-# string.ascii_lowercase is 'abcdefghijklmnopqrstuvwxyz'
-# string.ascii_uppercase is 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-# string.ascii_letters is ascii_lowercase + ascii_uppercase
-# string.printable is digits + ascii_letters + punctuation + whitespace
 
 
 def decode(digits, base):
@@ -17,13 +9,15 @@ def decode(digits, base):
     return: int -- integer representation of number (in base 10)"""
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
-    # TODO: Decode digits from binary (base 2)
-    # ...
-    # TODO: Decode digits from hexadecimal (base 16)
-    # ...
-    # TODO: Decode digits from any base (2 up to 36)
-    # ...
 
+    len_of_num = len(digits) - 1
+    dec_num = 0
+    hexatri_string = string.digits + string.ascii_lowercase
+    for digit in digits.lower():
+        dec_num += hexatri_string.index(digit) * (base ** len_of_num)
+        len_of_num -= 1
+
+    return dec_num
 
 def encode(number, base):
     """Encode given number in base 10 to digits in given base.
@@ -34,13 +28,18 @@ def encode(number, base):
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
     # Handle unsigned numbers only for now
     assert number >= 0, 'number is negative: {}'.format(number)
-    # TODO: Encode number in binary (base 2)
-    # ...
-    # TODO: Encode number in hexadecimal (base 16)
-    # ...
-    # TODO: Encode number in any base (2 up to 36)
-    # ...
 
+    hexatri_string = string.digits + string.ascii_lowercase
+    rem_arr = []
+    while number != 0:
+        dec, whole = modf(number/base)
+        rem_arr = [round(dec*base)] + rem_arr
+        number = whole
+
+    for i, num in enumerate(rem_arr):
+        rem_arr[i] = hexatri_string[int(num)]
+
+    return ''.join(rem_arr)
 
 def convert(digits, base1, base2):
     """Convert given digits in base1 to digits in base2.
@@ -51,15 +50,12 @@ def convert(digits, base1, base2):
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base1 <= 36, 'base1 is out of range: {}'.format(base1)
     assert 2 <= base2 <= 36, 'base2 is out of range: {}'.format(base2)
-    # TODO: Convert digits from base 2 to base 16 (and vice versa)
-    # ...
-    # TODO: Convert digits from base 2 to base 10 (and vice versa)
-    # ...
-    # TODO: Convert digits from base 10 to base 16 (and vice versa)
-    # ...
-    # TODO: Convert digits from any base to any base (2 up to 36)
-    # ...
 
+    if base1 == 10:
+        return encode(int(digits), base2)
+    else:
+        dec_digits = decode(digits, base1)
+        return str(encode(int(dec_digits), base2))
 
 def main():
     """Read command-line arguments and convert given digits between bases."""
@@ -76,6 +72,6 @@ def main():
         print('Usage: {} digits base1 base2'.format(sys.argv[0]))
         print('Converts digits from base1 to base2')
 
-
 if __name__ == '__main__':
     main()
+    
